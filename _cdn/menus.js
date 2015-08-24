9156ab04a2b4cc1a53fd5838c729e3e7e90040d4
adrianosilvareis <adriano_silvareis@hotmail.com>
2015-08-24 15:19:57 -0300

    script menu

--- _cdn/menus.js
+++ _cdn/menus.js
@@ -1,26 +1,8 @@
 $(function () {
 
-    //page = primeira palavra depois do nome do site
-    url = window.location.href.toString().split(window.location.host)[1];
-    pagina = url.split('/ciddhc/')[1];
-    page = pagina.split('/')[1];
-    
-    menu = $('.menu_js li');
-    menu.removeClass('active');
-
-    switch (page) {
-        case 'membros':
-            $('#membros').addClass('active');
-            break;
-        case 'contato':
-            $('#contato').addClass('active');
-            break;
-        case 'institucional':
-            $('#institucional').addClass('active');
-            break;
-        default:
-            $('#home').addClass('active');
-            break;
-    }
+    $(".menu_js li").click(function() {
+        $(".menu_js li").removeClass('active');
+        $(this).addClass('active');
+    });
 
 });
